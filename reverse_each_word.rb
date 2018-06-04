770731c07d8f1e5a32490a100178ece34834d792
Anusha Konatala <anu.konatala@gmail.com>
2018-06-04 06:28:23 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,12 @@
 def reverse_each_word(string)
   array = string.split(' ')
-  array.collect do |word|
+  
+  array.each do |word|
     word.reverse
   end
+  #array.collect do |word|
+  #  word.reverse
+  #end
 end
 
 puts "#{reverse_each_word("Hello there, and how are you?")}"
